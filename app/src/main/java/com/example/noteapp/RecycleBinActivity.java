7c430fba1d4e301a67c09b01ce16049ee559defa
quangdang <quangnttwin@gmail.com>
2022-05-22 13:54:26 +0700

    thung rac

---
+++ app/src/main/java/com/example/noteapp/RecycleBinActivity.java
@@ -0,0 +1,14 @@
+package com.example.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecycleBinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_bin);
+    }
+}
